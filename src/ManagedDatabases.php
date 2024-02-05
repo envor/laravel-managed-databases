f1fc3046b316b42622542f293c33a524970febb9
inmanturbo <inmanturbo@users.noreply.github.com>
2024-02-05 20:54:18 +0000

    Fix styling

--- src/ManagedDatabases.php
+++ src/ManagedDatabases.php
@@ -6,27 +6,27 @@ class ManagedDatabases
 {
     public static function configureManagedConnections()
     {
-        config(['database.connections.managed_sqlite' => array_merge(config('database.connections.managed_sqlite','database.connections.sqlite'), [
+        config(['database.connections.managed_sqlite' => array_merge(config('database.connections.managed_sqlite', 'database.connections.sqlite'), [
             'database' => null,
         ])]);
 
-        config(['database.connections.managed_mysql' => array_merge(config('database.connections.managed_mysql','database.connections.mysql'), [
+        config(['database.connections.managed_mysql' => array_merge(config('database.connections.managed_mysql', 'database.connections.mysql'), [
             'database' => null,
         ])]);
 
-        config(['database.connections.managed_mariadb' => array_merge(config('database.connections.managed_mysql','database.connections.mariadb'), [
+        config(['database.connections.managed_mariadb' => array_merge(config('database.connections.managed_mysql', 'database.connections.mariadb'), [
             'database' => null,
         ])]);
     }
 
     public static function configureSystemConnections()
     {
-        config(['database.connections.system_sqlite' => config('database.connections.system_sqlite','database.connections.sqlite')]);
+        config(['database.connections.system_sqlite' => config('database.connections.system_sqlite', 'database.connections.sqlite')]);
 
         config(['database.connections.system_sqlite.database' => ':memory:']);
 
-        config(['database.connections.system_mysql' => config('database.connections.mysql','database.connections.mysql')]);
+        config(['database.connections.system_mysql' => config('database.connections.mysql', 'database.connections.mysql')]);
 
-        config(['database.connections.system_mariadb' => config('database.connections.system_mariadb','database.connections.mariadb')]);
+        config(['database.connections.system_mariadb' => config('database.connections.system_mariadb', 'database.connections.mariadb')]);
     }
 }
